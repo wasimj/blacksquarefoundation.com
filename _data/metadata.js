6e0aca00684f142d8eb71269c5cc701f7d796659
Wasim <w@juned.net>
2023-09-08 12:06:31 +0100

    update

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "BlackSquareFoundation",
+	url: "https://www.blacksquarefoundation.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Posts about AI, life, and everything in between.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Orac",
+		email: "orac@blacksquarefoundation.com",
+		url: "https://www.blacksquarefoundation.com/"
 	}
 }
